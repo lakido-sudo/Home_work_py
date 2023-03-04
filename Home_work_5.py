d1a3f3c851672c31422141316d37975ac87237f4
lakido <lakido551@gmail.com>
2023-03-04 14:17:58 +0300

    +sum(a,b)

--- Home_work_5.py
+++ Home_work_5.py
@@ -5,12 +5,26 @@ B = int(input("Введите его степень B: "))
 
 
 def stepen(A, B):
-    if (B == 1):
-        return (A)
-    elif (B == 0):
+    if B == 1:
+        return A
+    elif B == 0:
         return 1
-    if (B != 1):
-        return (A * stepen(A, B - 1))
+    if B != 1:
+        return A * stepen(A, B - 1)
 
 
 print("Результат возведения в степень равен: ", stepen(A, B))
+
+# Рекурсия. sum(a, b)
+
+a = int(input('Введите а: '))
+b = int(input('Введите b: '))
+
+
+def summa(a, b):
+    if a == 0:
+        return b
+    return summa(a-1, b+1)
+
+
+print(summa(a, b))
